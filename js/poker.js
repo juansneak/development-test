863f30c9ac8eb73ef85e337975d1a1613a0c35b4
Juan Carlos Morantes Taccetti <juansneak@gmail.com>
2017-10-01 14:25:39 -0300

    Adding more comments

--- js/poker.js
+++ js/poker.js
@@ -20,9 +20,9 @@ function Poker(){
 		var deferred = jQuery.Deferred();
 		var result = deferred.promise();
 		var index;
-		var numbers = [];
-		var orderedNumbers = [];
-		var suits = [];
+		var numbers = []; //contains all cards of the hand sorted by numbers
+		var orderedNumbers = []; 
+		var suits = []; //contains all cards of the hand sorted by suits
 		var hand = [];
 		var values = ['0','1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 		api.deal(deck,amount).done(function(data){
